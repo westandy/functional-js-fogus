c02b0d0943484d698e51dd45b1f5409e674014cc
Andy Westmeyer <andy.westmeyer@alteryx.com>
2020-06-05 15:10:43 -0600

    Made changes to myLength and dynamicProgramming for conversation

--- Chapter6/dynamicProgramming.js
+++ Chapter6/dynamicProgramming.js
@@ -8,20 +8,20 @@ const fibonacciNaive = nth => {
   return fibonacciNaive(nth - 1) + fibonacciNaive(nth - 2);
 };
 
-// console.log(fibonacciNaive(20));
+console.log(fibonacciNaive(20));
 
 /**
  * Dynamic combined with Recursion to avoid a stack overflow
  * From 2^N stack operations to N stack operations (still 2^N function calls)
  */
-let mem = {};
+let heapMemory = {};
 const fibonacciDynamic = nth => {
   if (nth < 2) {
     return 1;
   }
-  if (!mem[nth]) {
-    mem[nth] = fibonacciDynamic(nth - 1) + fibonacciDynamic(nth - 2);
+  if (!heapMemory[nth]) {
+    heapMemory[nth] = fibonacciDynamic(nth - 1) + fibonacciDynamic(nth - 2);
   }
-  return mem[nth];
+  return heapMemory[nth];
 };
 console.log(fibonacciDynamic(1200));
--- Chapter6/myLength.js
+++ Chapter6/myLength.js
@@ -1,16 +1,33 @@
 const existy = require('../Chapter2/existy');
+const isEmpty = require('../Chapter2/isEmpty');
 
 const myLength = array => {
-  // if ( _.isEmpty(ary) )
   if (!existy(array) || !array[0]) {
     return 0;
   } else {
-    // _.rest(ary)
     const rest = array.slice(1);
     return 1 + myLength(rest);
   }
 };
 
 // console.log(myLength([1, 2, 3, 4, 5]));
+
+const myLengthIterative = array => {
+  if (!existy(array) || !array[0]) return 0;
+
+  let CPU_STACK = [];
+  for (let ii = 0; !!array[ii]; ii++) {
+    CPU_STACK.push(ii);
+  }
+
+  let count = 0;
+  while (!isEmpty(CPU_STACK)) {
+    count++;
+    CPU_STACK.pop();
+  }
+  return count;
+};
+
+console.log(myLengthIterative([1, 2, 3, 4, 5]));
 
 module.exports = myLength;
